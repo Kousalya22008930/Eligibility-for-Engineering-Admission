25d1d396d9a8052333d7078185e65d1bf40f66a9
Kousalya22008930 <kousalyaashokkumar20@gamil.com>
2024-02-19 09:38:13 +0530

    Done

--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
 ﻿using System;
-    class Eligibility
+using System.Numerics;
+class Eligibility
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int maths, physics, chemistry,total;
+        Console.WriteLine("Enter the Maths mark:");
+        maths = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Physics mark");
+        physics= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Chemistry mark");
+        chemistry= Convert.ToInt32(Console.ReadLine());
+        total = maths + physics + chemistry;
+        if (maths >= 65 && physics >= 55 && chemistry >= 50)
         {
-           Console.WriteLine("Hiiiiiii");
+            if (total>= 180 || (maths + physics)>= 140)
+            {
+                Console.WriteLine("Eligible for admission with total marks of {0} out of 300",total);
+            }
+            else
+            {
+                Console.WriteLine("Not Eligible for admission with total marks of {0} out of 300",total);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Not Eligible for admission with total marks {0} out of 300",total);
         }
     }
+}
 
